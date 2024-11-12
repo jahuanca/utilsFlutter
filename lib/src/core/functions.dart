913360f91b3ac941e28e88cd838e5eb0d8f89085
jahuanca <41663809+jahuanca@users.noreply.github.com>
2024-11-12 16:04:32 -0500

    mend

--- lib/src/core/functions.dart
+++ lib/src/core/functions.dart
@@ -2,11 +2,11 @@
 Map<T, List<S>> groupBy<S, T>(
   {
     required Iterable<S> values, 
-    required T Function(S) key
+    required T Function(S) functionKey
   }) {
   Map<T, List<S>> map = <T, List<S>>{};
   for (S element in values) {
-    (map[key(element)] ??= []).add(element);
+    (map[functionKey(element)] ??= []).add(element);
   }
   return map;
 }
