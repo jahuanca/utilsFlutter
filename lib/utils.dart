ff605a1b7778419b1aa09f77d426f80c503d8654
jahuanca <41663809+jahuanca@users.noreply.github.com>
2024-12-09 13:14:19 -0500

    mend

--- lib/utils.dart
+++ lib/utils.dart
@@ -12,6 +12,7 @@ export 'src/core/functions.dart';
 export 'src/ui/widgets/inputs/input_widget.dart';
 export 'src/ui/widgets/inputs/password_input_widget.dart';
 export 'src/ui/widgets/buttons/button_widget.dart';
+export 'src/ui/widgets/icons/icon_widget.dart';
 export 'src/ui/widgets/buttons/icon_button_widget.dart';
 export 'src/ui/widgets/app_bar/app_bar_widget.dart';
 export 'src/ui/widgets/images/image_action_widget.dart';
