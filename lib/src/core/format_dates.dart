63b313d9975ddbaa7f766bf43f2c48c6ecf4ee63
jahuanca <41663809+jahuanca@users.noreply.github.com>
2024-12-19 23:28:35 -0500

    mend

--- lib/src/core/format_dates.dart
+++ lib/src/core/format_dates.dart
@@ -3,6 +3,9 @@ import 'package:intl/intl.dart';
 extension DateFormats on DateTime? {
   String? formatDMMYYY() =>
       this == null ? null : DateFormat(onlyDateFormat).format(this!);
+
+  String? format(String formatDate) =>
+      this == null ? null : DateFormat(formatDate).format(this!);
 }
 
 const String onlyDateFormat = 'd/M/y';
---
+++ lib/src/core/internationalizations.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates = [
+  GlobalCupertinoLocalizations.delegate,
+  GlobalMaterialLocalizations.delegate,
+  GlobalWidgetsLocalizations.delegate,
+];
+
+
