19f42d1ed8ed85bcb439e1e01d15f86ec71d712f
jahuanca <41663809+jahuanca@users.noreply.github.com>
2024-12-19 23:09:10 -0500

    mend

--- lib/src/core/format_dates.dart
+++ lib/src/core/format_dates.dart
@@ -1,8 +1,8 @@
 import 'package:intl/intl.dart';
 
 extension DateFormats on DateTime? {
-  String? getOnlyDates() =>
+  String? formatDMMYYY() =>
       this == null ? null : DateFormat(onlyDateFormat).format(this!);
 }
 
-const String onlyDateFormat = 'd/mm/yyyy';
+const String onlyDateFormat = 'd/M/y';
