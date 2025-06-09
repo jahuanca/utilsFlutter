867f4537cf7d2e3d98fdf463bedf99c4fa8c70ff
jahuanca <41663809+jahuanca@users.noreply.github.com>
2025-06-09 17:17:14 -0500

    feat: add hero tags

---
+++ lib/src/core/hero_tags.dart
@@ -0,0 +1,5 @@
+
+const String addHeroTag = 'addHeroTag';
+const String editHeroTag = 'editHeroTag';
+const String deleteHeroTag = 'deleteHeroTag';
+const String analyticsHeroTag = 'analyticsHeroTag';
