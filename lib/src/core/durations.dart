28aa0e943704d490773b9e42c2dd3906e7c39a99
jahuanca <41663809+jahuanca@users.noreply.github.com>
2025-06-14 23:49:08 -0500

    feat: add comments in constans duration

--- lib/src/core/durations.dart
+++ lib/src/core/durations.dart
@@ -1,6 +1,10 @@
-
+/// Duracion de 1 día.
 const Duration oneDayDuration = Duration(days: 1);
+/// Duracion de una semana.
 const Duration oneWeekDuration = Duration(days: 7);
+/// Duracion de 15 días.
 const Duration fortnightDuration = Duration(days: 15);
+/// Duracion de medio anio o 180 días
 const Duration halfYearDuration = Duration(days: 180);
+/// Duracion de 1 anio o 360 días
 const Duration oneYearDuration = Duration(days: 360);
