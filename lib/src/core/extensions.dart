d69e3b7102f4dfc6925549a01f50584cf668b780
jahuanca <41663809+jahuanca@users.noreply.github.com>
2025-06-19 00:44:13 -0500

    feat: add truncateAndFormatDecimals

--- lib/src/core/extensions.dart
+++ lib/src/core/extensions.dart
@@ -1,8 +1,17 @@
+import 'dart:math';
 import 'package:utils/utils.dart';
 
 extension Double on double {
   String formatDecimals([int numberDecimals = numberDecimalsDefault]) =>
       (this - truncate() != 0) ? toStringAsFixed(numberDecimals) : toStringAsFixed(defaultInt);
+  
+  String truncateAndFormatDecimals([int numberDecimals = numberDecimalsDefault]) {
+    num mod = pow(10.0, numberDecimals);
+    double value = ((this * mod).round().toDouble() / mod);
+    return (value - truncate() != 0) 
+      ? value.toStringAsFixed(numberDecimals) 
+      : value.toStringAsFixed(defaultInt);
+  }
 }
 
 extension StringExtensions on String {
