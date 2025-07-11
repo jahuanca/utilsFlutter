4400937025991f02ca4855a8a96292f3acc535db
jahuanca <41663809+jahuanca@users.noreply.github.com>
2025-07-10 23:39:16 -0500

    fix: firstDate and LastDate of the week

--- lib/src/core/extensions/datetime_extensions.dart
+++ lib/src/core/extensions/datetime_extensions.dart
@@ -2,10 +2,26 @@
 import 'package:utils/src/core/default_extensions.dart';
 
 extension CalculateTime on DateTime? {
-  DateTime lastDateOfTheWeek() => firstDateOfTheWeek()
-    .add(const Duration(days: DateTime.daysPerWeek - 1));
+  DateTime lastDateOfTheWeek() {
+    
+    return firstDateOfTheWeek()
+      .finalOfDay()
+      .add(const Duration(days: DateTime.daysPerWeek - 1));
+  }
 
-  DateTime firstDateOfTheWeek() => orNow()
-    .subtract(Duration(days: orNow().weekday - 1));
+  DateTime firstDateOfTheWeek() {
+    DateTime dateTime = orNow().initialOfDay();
+    return dateTime.subtract(Duration(days: orNow().weekday - 1));
+  }
+
+  DateTime initialOfDay() {
+    DateTime dateTime = orNow();
+    return DateTime(dateTime.year, dateTime.month, dateTime.day, 0, 0, 0);
+  }
+
+  DateTime finalOfDay(){
+    DateTime dateTime = orNow();
+    return DateTime(dateTime.year, dateTime.month, dateTime.day, 23, 59, 59);
+  }
 
 }
