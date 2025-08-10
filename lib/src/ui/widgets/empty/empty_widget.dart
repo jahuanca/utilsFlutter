5167ef109cbc3484d0b494eddc41c7eb66ec4da0
jahuanca <41663809+jahuanca@users.noreply.github.com>
2025-08-09 19:52:11 -0500

    feat: update values of string in EmptyWidget

--- lib/src/ui/widgets/empty/empty_widget.dart
+++ lib/src/ui/widgets/empty/empty_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:utils/utils.dart';
 import 'package:utils/src/ui/utils/strings.dart';
 
 class EmptyWidget extends StatelessWidget {
@@ -9,15 +10,17 @@ class EmptyWidget extends StatelessWidget {
   final IconData iconData;
   final double iconSize;
   final Color? iconColor;
+  final bool showIcon;
 
   EmptyWidget({
     super.key,
-    this.title,
-    this.description,
+    this.title = titleOfEmptyWidgetString,
+    this.description = descriptionOfEmptyWidgetString,
     this.padding,
     this.iconData = Icons.hourglass_empty,
     this.iconSize = 60,
     this.iconColor,
+    this.showIcon = true,
   });
 
   @override
@@ -38,11 +41,15 @@ class EmptyWidget extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          if (showIcon)
           Icon(iconData, size: iconSize, color: iconColor,),
           SizedBox(height: 5,),
-          Text(title ?? titleOfEmptyWidgetString, style: titleStyle,),
+          if (title != null)
+          Text(title.orEmpty(), style: titleStyle,),
+          if (title != null)
           SizedBox(height: 10,),
-          Text(description ?? descriptionOfEmptyWidgetString, textAlign: TextAlign.center,),
+          if (description != null)
+          Text(description.orEmpty(), textAlign: TextAlign.center,),
         ],
       ),
     );
