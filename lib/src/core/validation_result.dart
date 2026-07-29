657d5ed38621cc46fe5dd6326b8d2d33318cd046
jahuanca <41663809+jahuanca@users.noreply.github.com>
2026-07-29 13:30:14 -0500

    fix: add method that init values for validationResult

--- lib/src/core/validation_result.dart
+++ lib/src/core/validation_result.dart
@@ -1,7 +1,7 @@
 
 /// Estructura que retorna el metodo [validateText]
 
-class ValidateResult {
+class ValidateResult<T> {
   /// `hasError` devuelve true o false en caso exista un error
   bool hasError;
 
@@ -9,11 +9,16 @@ class ValidateResult {
   String? error;
 
   /// `value` en caso de no existir un error contiene el valor.
-  dynamic value;
+  T? value;
 
   ValidateResult({
     this.hasError = true,
     this.error = 'Este campo aún no ha sido validado',
     required this.value,
   });
+
+  factory ValidateResult.toInit(T value) => ValidateResult(
+    hasError: false,
+    value: value,
+  );
 }
